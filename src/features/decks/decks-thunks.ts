1f92090c83c025f14c2aa927dc26272d65e4a4fe
apelsinom <apelsinom@mail.ru>
2024-03-03 22:55:25 +0300

    3.2 - "Обработка ошибок: Решение"

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { setAppStatusAC } from '../../app/app-reducer.ts'
+import { isAxiosError } from 'axios'
 
 export const fetchDecksTC = () => async (dispatch: Dispatch) => {
   dispatch(setAppStatusAC('loading'))
@@ -27,7 +28,20 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
 }
 
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
-    dispatch(updateDeckAC(res.data))
-  })
+  try {
+    throw new Error('Boooooom!')
+    const res = await decksAPI.updateDeck(params)
+      dispatch(updateDeckAC(res.data))
+  } catch (e) {
+    let ourMessageError: string
+    if (isAxiosError<ServerError>(e)) {
+      ourMessageError = e.response ? e.response.data.errorMessages[0].message : e.message
+    } else {
+      ourMessageError = (e as Error).message
+    }
+      console.log(ourMessageError)
+  }
+}
+type ServerError = {
+  errorMessages: Array<{field: string; message: string}>
 }
